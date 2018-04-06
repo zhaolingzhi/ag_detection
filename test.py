cc79a1af2bfd47f675f199c3c23aef386c7b0e18
zhaolingzhi <1130262279@qq.com>
2018-04-06 22:25:20 +0800

    test the performance

--- test.py
+++ test.py
@@ -1,36 +1,25 @@
+#encoding:utf-8
 import numpy as np
 import keras
+import scipy as sc
+from input import *
+from net_model import *
 from keras.models import Sequential
-from keras.layers import Dense, Dropout, Flatten
-from keras.layers import Conv2D, MaxPooling2D
+from keras.layers import Dense,Dropout,Flatten
+from keras.layers import Conv2D,MaxPooling2D
 from keras.optimizers import SGD
+from keras import initializers
 
-# Generate dummy data
-x_train = np.random.random((100, 101, 101, 3))
-y_train = keras.utils.to_categorical(np.random.randint(10, size=(100, 1)), num_classes=10)
-x_test = np.random.random((20, 101, 101, 3))
-y_test = keras.utils.to_categorical(np.random.randint(10, size=(20, 1)), num_classes=10)
+log_info="log_info.txt"
+path="/home/zlz/PycharmProjects/ag_detection/model.h5"
+batch_size=50
+epochs=100
 
-model = Sequential()
-# input: 100x100 images with 3 channels -> (100, 100, 3) tensors.
-# this applies 32 convolution filters of size 3x3 each.
-model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(101, 101, 3)))
-model.add(Conv2D(32, (3, 3), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
 
-model.add(Conv2D(64, (3, 3), activation='relu'))
-model.add(Conv2D(64, (3, 3), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
-
-model.add(Flatten())
-model.add(Dense(256, activation='relu'))
-model.add(Dropout(0.5))
-model.add(Dense(10, activation='softmax'))
-
-sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
-model.compile(loss='categorical_crossentropy', optimizer=sgd)
-
-model.fit(x_train, y_train, batch_size=32, epochs=10)
-score = model.evaluate(x_test, y_test, batch_size=32)
+file=open('//home//zlz//PycharmProjects//ag_detection//Folds//my_image.txt','r')
+list=get_list(file)
+my_test=read_image(list)
+model=net_model('Alexnet-simple-gender')
+model.load_weights("model.h5")
+result=model.predict(my_test)
+print result
